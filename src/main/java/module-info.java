0a7684bd79d617b766e4d385a5bd6f2fabb49d70
StrangerCoug <strangercoug@hotmail.com>
2019-08-29 12:55:29 -0600

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.github.strangercoug.freecasino {
+	exports com.github.strangercoug.freecasino;
+	exports com.github.strangercoug.freecasino.enums;
+	exports com.github.strangercoug.freecasino.games.poker.video;
+	exports com.github.strangercoug.freecasino.exceptions;
+	exports com.github.strangercoug.freecasino.games.electronic;
+	exports com.github.strangercoug.freecasino.games;
+	exports com.github.strangercoug.freecasino.games.table;
+	exports com.github.strangercoug.freecasino.games.poker;
+	exports com.github.strangercoug.freecasino.objs;
+}
